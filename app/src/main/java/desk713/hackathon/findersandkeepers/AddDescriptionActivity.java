4b297fdaab4126db130b17a7014d96a063a8a254
Anna Trofimova <anna.trofimova91@gmail.com>
2018-08-04 16:41:40 +1000

    layouts initiated

--- app/src/main/java/desk713/hackathon/findersandkeepers/AddDescriptionActivity.java
+++ app/src/main/java/desk713/hackathon/findersandkeepers/AddDescriptionActivity.java
@@ -1,16 +1,13 @@
 package desk713.hackathon.findersandkeepers;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-    Button buttonFound;
-    Button buttonLost;
+public class AddDescriptionActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_add_description);
     }
 }
--- app/src/main/java/desk713/hackathon/findersandkeepers/TakePictureActivity.java
+++ app/src/main/java/desk713/hackathon/findersandkeepers/TakePictureActivity.java
@@ -1,16 +1,14 @@
 package desk713.hackathon.findersandkeepers;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-    Button buttonFound;
-    Button buttonLost;
+public class TakePictureActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_take_picture);
     }
 }
